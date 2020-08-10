9060cebb92f12d55f927a0fc9ba7dad58eb3a6ed
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-11 04:41:29 +0530

    stack frames, store to experience replay

--- agent.py
+++ agent.py
@@ -8,22 +8,56 @@ from nnet_gpu import functions
 import numpy as np
 import cupy as cp
 
-class ReplayMemory:
-	def __init__(self, capacity=10000)
 
+WIDTH = 80
+HEIGHT = 80
+NFRAMES = 4
+
+
+def preprocess_observation(observation):
+	state = observation[34:194] # 160,160,3
+	state = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
+	state = cv2.resize(state, (HEIGHT,WIDTH), interpolation = cv2.INTER_NEAREST)
+	state = np.expand_dims(state, axis=2)
+	return state
+
+
+class ReplayMemory:
+	def __init__(self, capacity=10000):
+		self.capacity = capacity
+		self.current_state = np.zeros((self.capacity, HEIGHT, WIDTH, NFRAMES), dtype=np.uint8)
+		self.action = np.zeros(self.capacity, dtype=np.uint8)
+		self.reward = np.zeros(self.capacity, dtype=np.uint8)
+		self.next_state = np.zeros_like(self.current_state)
+		self.done = np.zeros(self.capacity, dtype=np.bool)
+		self.idx = 0
+		self.len = 0
+
+
+	def store_transition(self, cur_state, action, reward, nxt_state, done):
+		self.current_state[self.idx] = cur_state
+		self.action[self.idx] = action
+		self.reward[self.idx] = reward
+		self.next_state[self.idx] = nxt_state
+		self.done[self.idx] = done
+
+		self.idx = (self.idx + 1) % self.capacity
+		if self.len < self.capacity:
+			self.len+= 1
+
+
+	def sample_random(batch_size):
+		i = np.random.choice(self.len, batch_size)
+		return self.current_state[i], self.action[i], self.reward[i], self.next_state[i], self.done[i]
 
 class Agent:
-	def __init__(self, actions=[0,1], epsilon=1, min_epsilon=0.1):
+	def __init__(self, actions=[0,2,3], epsilon=1, min_epsilon=0.1, eps_decay=9e-5):
 		self.epsilon = epsilon
 		self.min_epsilon = min_epsilon
+		self.eps_decay = eps_decay
 		self.actions = actions
-		self.model = self.get_model(no_of_actions=len(self.actions))
+		self.model = self.get_model(input_shape=(WIDTH,HEIGHT,NFRAMES), no_of_actions=len(self.actions))
 
-
-	def preprocess(self, img):
-		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-		img = cv2.resize(img, (80,80), interpolation = cv2.INTER_NEAREST)
-		return img
 
 	def get_action(self, state, epsilon=0.1):
 		if np.random.uniform() <= epsilon: # random action with epsilon greedy
@@ -33,15 +67,15 @@ class Agent:
 			return actions[np.argmax(out[0])]
 
 	@staticmethod
-	def get_model(input_shape=(80,80,4), no_of_actions=4):
+	def get_model(input_shape=(WIDTH,HEIGHT,NFRAMES), no_of_actions=3):
 		model=Sequential()
 		model.add(Conv2D(num_kernels=32, kernel_size=3, stride=(2, 2), activation=functions.relu, input_shape=input_shape))
 		model.add(Dropout(0.1))
 		model.add(Conv2D(num_kernels=64, kernel_size=3, stride=(2, 2), activation=functions.relu))
 		model.add(Dropout(0.2))
 		model.add(Conv2D(num_kernels=128, kernel_size=3, stride=(2, 2), activation=functions.relu))
-		model.add(Dropout(0.3))
 		model.add(Flatten())
+		model.add(Dropout(0.3))
 		model.add(Dense(256, activation=functions.relu))
 		model.add(Dense(no_of_actions, activation=functions.echo))
 
--- game_environment.py
+++ game_environment.py
@@ -1,20 +1,35 @@
 import gym
 import time
 import cv2
-# from agent import Agent
+import numpy as np
+from agent import *
 
 env = gym.make('Pong-v0')
 
+D_exp = ReplayMemory(capacity=10000)
+
 for i_episode in range(2):
+	stacked_state = np.zeros((NFRAMES,HEIGHT,WIDTH,1), dtype=np.uint8)
+	stacked_next = np.zeros_like(stacked_state)
 	observation = env.reset()
+	state = preprocess_observation(observation)
+	stacked_next[:3] = stacked_next[1:]
+	stacked_next[3] = state
 	ep_score = 0
 	for t in range(10000):
 		# env.render()
-		state = observation[34:194] # 160,160,3
+		stacked_state[:] = stacked_next			# copy values of now current state
 		action = env.action_space.sample()
 		next_observation, reward, done, info = env.step(action)
 		ep_score += reward
 		observation = next_observation
+
+		state = preprocess_observation(observation)
+		stacked_next[:3] = stacked_next[1:]
+		stacked_next[3] = state
+
+		D_exp.store_transition(stacked_state, action, reward, stacked_next, done)
+
 		if done:
 			print("Episode finished after {} timesteps".format(t+1))
 			break
