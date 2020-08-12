e174a3f27cd425dcacac2da65b0afe618df5f39f
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-12 23:28:47 +0530

    linear

--- agent.py
+++ agent.py
@@ -10,22 +10,22 @@ import cupy as cp
 
 from settings import *
 
-## Tanh was changed, overlap and past reward was changed
+## Tanh was changed, overlap and past reward was changed, try overlap of NFRAMES - 1 or - 2
 
 
 def get_model(input_shape=(HEIGHT,WIDTH,NFRAMES), no_of_actions=3):
 	model=Sequential()
 	model.add(Conv2D(num_kernels=32, kernel_size=3, stride=(2, 2), activation=functions.relu, input_shape=input_shape))
-	# model.add(BatchNormalization())
+	model.add(BatchNormalization())
 	model.add(Conv2D(num_kernels=64, kernel_size=3, stride=(2, 2), activation=functions.relu))
-	# model.add(BatchNormalization())
+	model.add(BatchNormalization())
 	model.add(Conv2D(num_kernels=128, kernel_size=3, stride=(2, 2), activation=functions.relu))
-	# model.add(BatchNormalization())
+	model.add(BatchNormalization())
 	model.add(Flatten())
 	model.add(Dense(256, activation=functions.relu))
-	model.add(Dense(no_of_actions, activation=functions.echo))
+	model.add(Dense(no_of_actions, activation=functions.linear))
 
-	model.compile(optimizer=optimizers.adam, loss=functions.mean_squared_error, learning_rate=0.00001)
+	model.compile(optimizer=optimizers.adam, loss=functions.mean_squared_error, learning_rate=0.00005)
 	return model
 
 
@@ -60,15 +60,15 @@ class Agent:
 	def train(self, D_exp, batch_size=BATCH_SIZE, gamma=0.99):
 		curr_state, action_idxs, rewards, next_state, not_done = D_exp.sample_random(batch_size)
 		curr_gpu = state_to_gpu(curr_state)
-		# Qar = self.model.predict(curr_gpu)							# predict reward for current state
+		# Y_t = self.model.predict(curr_gpu)							# predict reward for current state
 		
 		Qar_next = self.model.predict(state_to_gpu(next_state))		# predict reward for next state
 		Qr_next  = Qar_next.max(axis=1)								# get max rewards (greedy)
 		Qr_next  = Qr_next * cp.asarray(not_done, dtype=cp.float32)	# zero out next rewards for terminal
 		Y_argm   = cp.asarray(rewards, dtype=cp.float32) + gamma*Qr_next
 
-		Qar = cp.zeros_like(Qar_next)
-		Qar[np.arange(len(curr_state)), action_idxs] = Y_argm
-		self.model.train_on_batch(curr_gpu, Qar)
+		Y_t = cp.zeros_like(Qar_next)
+		Y_t[np.arange(len(curr_state)), action_idxs] = Y_argm
+		self.model.train_on_batch(curr_gpu, Y_t)
 
 	
