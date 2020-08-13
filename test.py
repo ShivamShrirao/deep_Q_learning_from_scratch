d251fe5a663e74f5316f8d22c4245232331fa537
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-13 07:55:47 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+import gym
+import time
+import numpy as np
+
+from settings import *
+from agent import *
+from experience import *
+from atari_wrappers import *
+
+fps = 60
+
+agt = Agent(actions=[0,2,3], epsilon=1)
+agt.model.load_weights("model.w8s")
+
+env = gym.make('Pong-v0')
+env = FrameStack(env, NFRAMES)      # preprocess and stack frames
+
+for i_episode in range(5):
+    observation = env.reset()
+    ep_score = 0
+    start = time.time()
+    for t in range(10000):
+        env.render()
+        action = agt.get_action(observation)
+        next_observation, reward, done, info = env.step(action)
+        ep_score += reward
+        observation = next_observation
+        # time.sleep(1/fps)
+        if done:
+            break
+        print('\r', t, action, ep_score, end='  ')
+    print(f"\rEpisode {i_episode+1} finished after {t+1} timesteps, Score: {ep_score}, Epsilon: {agt.epsilon:.6f}, Time: {time.time()-start:.2f}")
+env.close()
