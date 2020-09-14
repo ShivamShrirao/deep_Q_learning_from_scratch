7c1dc0d805a70c0c7e4fcbbb58eaac3185c5792f
ShivamShrirao <shivamshrirao@gmail.com>
2020-09-14 20:34:58 +0530

    comments added

--- agent.py
+++ agent.py
@@ -6,6 +6,8 @@ import cv2
 
 from settings import *
 
+# TODO - https://arxiv.org/pdf/1710.02298.pdf
+
 def state_to_gpu(state):
 	return cp.asarray(state, dtype=cp.float32)/127.5 - 1
 
@@ -21,20 +23,20 @@ def sample_to_gpu(curr_state, action_idxs, rewards, next_state, not_done):
 class BaseAgent:
 	def __init__(self, actions=[0,2,3], epsilon=1, min_epsilon=0.1, eps_decay=2e-6, target_update_thresh=1000, grad_clip=True, continue_decay=False):
 		self.epsilon = epsilon
-		self.min_epsilon = min_epsilon
-		self.eps_decay = eps_decay
-		self.actions = actions
+		self.min_epsilon = min_epsilon                          # minimum value for epsilin
+		self.eps_decay = eps_decay                              # decay rate for epsilon
+		self.actions = actions 									# actions in an array
 		self.grad_clip = grad_clip								# whether to clip gradients
 		self.continue_decay = continue_decay
 		self.target_update_counter = 0
-		self.target_update_thresh = target_update_thresh
+		self.target_update_thresh = target_update_thresh        # interval of updating target model
 		self.stream = stream_maps.get_next_stream()
 
-	def predict(self, state_que):								# input the observations of length NFRAMES
+	def predict(self, state_que):								# input the observations(state_que) of length NFRAMES
 		state = state_to_gpu(state_que)
 		state = cp.moveaxis(state, 0, -1)
 		state = cp.expand_dims(state, axis=0)
-		return self.model.predict(state)
+		return self.model.predict(state)						# predict output
 
 
 	def get_action(self, state_que):
@@ -48,7 +50,7 @@ class BaseAgent:
 		if np.random.uniform() <= self.epsilon:					# random action with epsilon greedy
 			return np.random.choice(self.actions)
 		else:
-			out = self.predict(state_que)
+			out = self.predict(state_que)						# Else model predicts action
 			return self.actions[cp.argmax(out[0]).item()]
 
 
